7abcc7ebfa322d41127d302635ac1abaa04a9ef3
XiaoLei Li <707913055@qq.com>
2012-03-12 22:35:57 +0800

    devidetoke的保存到uidefault中，登录时传递

--- Projects/hrms/hrms/AppDelegate.h
+++ Projects/hrms/hrms/AppDelegate.h
@@ -12,8 +12,4 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
-
-@property (strong, nonatomic) ViewController *viewController;
-
 @end
--- Projects/hrms/hrms/AppDelegate.m
+++ Projects/hrms/hrms/AppDelegate.m
@@ -8,35 +8,28 @@
 
 #import "AppDelegate.h"
 
-#import "ViewController.h"
-
 //styleSheet
 #import "MyStyleSheet.h"
 //view controllers
 #import "RCLoginViewController.h"
 //tab页面
-#import "CRMainController.h"
+//#import "CRMainController.h"
 //偏好设置
-#import "CRPreferencesController.h"
-//关于
-#import "RCAboutUsController.h"
+//#import "CRPreferencesController.h"
+////关于
+//#import "RCAboutUsController.h"
 //审批
 #import "ApproveListController.h"
 
 @implementation AppDelegate
 
-@synthesize window = _window;
-@synthesize viewController = _viewController;
-
-- (void)dealloc
+-(void) applicationDidFinishLaunching:(UIApplication *)application
 {
-    [_window release];
-    [_viewController release];
-    [super dealloc];
-}
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+    application.applicationIconBadgeNumber = 0;
+    //register notification
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeSound)];
+    
+    
     //Views Managment by Three20
     TTNavigator* navigator = [TTNavigator navigator];
     navigator.persistenceMode = TTNavigatorPersistenceModeAll;
@@ -53,51 +46,46 @@
     [map from:@"tt://login" toModalViewController:[RCLoginViewController class]];
     
     //main view
-    [map from:@"tt://main" toSharedViewController:[CRMainController class]];
+    //[map from:@"tt://main" toSharedViewController:[CRMainController class]];
     
-    [map from:@"tt://preferences/(initWithStyle:)"  toSharedViewController:[CRPreferencesController class]];
+//    [map from:@"tt://preferences/(initWithStyle:)" 
+//       parent:@"aprove"  
+//toSharedViewController:[CRPreferencesController class]];
     
-    [map from:@"tt://about" toSharedViewController:[RCAboutUsController class]];
+//    [map from:@"tt://about" 
+//       parent:@"tt://preferences/1" 
+//toSharedViewController:[RCAboutUsController class]];
     
     //审批
-    [map from:@"tt://aprove" parent:@"aprove" toSharedViewController:[ApproveListController class]];
-//    [map from:@"tt://menu/(initWithMenu:)" toSharedViewController:[MenuController class]];
+    [map from:@"tt://aprove" 
+       parent:@"tt://preferences/1" 
+toSharedViewController:[ApproveListController class]];
+    //[map from:@"tt://menu/(initWithMenu:)" toSharedViewController:[MenuController class]];
     
     
     
     if (![navigator restoreViewControllers]) {
         [navigator openURLAction:[TTURLAction actionWithURLPath:@"tt://login"]];
     }
-    return YES;
-//
-//    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-//        self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController_iPhone" bundle:nil] autorelease];
-//    } else {
-//        self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController_iPad" bundle:nil] autorelease];
-//    }
+}
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    //get the deivcetoken
+    NSLog(@"My token is: %@", deviceToken);
+    NSLog(@"%@",[[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]]); 
+    //save deviceToken to userDefaults
+    [[NSUserDefaults standardUserDefaults] setValue:[[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]] forKey:@"deviceToken"];
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"Error in registration.Error: %@" ,error);
+//    UIAlertView * alert = [[UIAlertView alloc]initWithTitle:@"提示" message:@"注册推送失败，检查网络或推送服务" delegate:self cancelButtonTitle:@"确认" otherButtonTitles: nil];
+//    [alert show];
+//    [alert release];
     
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[NSUserDefaults standardUserDefaults] setValue:nil forKey:@"deviceToken"];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
