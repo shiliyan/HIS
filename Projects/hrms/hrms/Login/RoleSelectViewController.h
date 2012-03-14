e36ed84d620dbe6c45a082906c6c4d442dfc15eb
XiaoLei Li <707913055@qq.com>
2012-03-14 09:25:49 +0800

    修改键盘弹出页面不能移动的bug，和request回调

---
+++ Projects/hrms/hrms/Login/RoleSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoleSelectViewController.h
+//  hrms
+//
+//  Created by Rocky Lee on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RoleSelectViewController : NSObject
+
+@end
---
+++ Projects/hrms/hrms/Login/RoleSelectViewController.m
@@ -0,0 +1,13 @@
+//
+//  RoleSelectViewController.m
+//  hrms
+//
+//  Created by Rocky Lee on 3/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RoleSelectViewController.h"
+
+@implementation RoleSelectViewController
+
+@end
