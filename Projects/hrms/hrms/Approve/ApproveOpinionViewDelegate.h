86d964d1d52082bcbb8b87421af70b7307c33c1e
Yi Zhang <zhemerson@gmail.com>
2012-03-15 17:16:33 +0800

    用于审批弹出窗的关闭delegate

---
+++ Projects/hrms/hrms/Approve/ApproveOpinionViewDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ApproveOpinionViewDelegate.h
+//  Approve
+//
+//  Created by Zhang Yi on 3/9/12.
+//  Copyright (c) 2012 __HAND__. All rights reserved.
+//  用于审批弹出窗的关闭delegate
+
+#import <Foundation/Foundation.h>
+
+@protocol ApproveOpinionViewDelegate <NSObject>
+
+@required
+-(void)ApproveOpinionViewDismissed:(int)resultCode;
+
+
+@end
