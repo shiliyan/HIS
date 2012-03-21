6333eed8a57f27f10dcaa50f1a508ee1ee452b22
Yi Zhang <zhemerson@gmail.com>
2012-03-21 10:39:43 +0800

    加入对应数据库表的列名常量

--- Projects/hrms/hrms/Approve/Approve.h
+++ Projects/hrms/hrms/Approve/Approve.h
@@ -8,28 +8,70 @@
 
 #import <Foundation/Foundation.h>
 
-static  NSString *TODO_TYPE_HURRY = @"HURRY";
-static  NSString *TODO_TYPE_NORMAL = @"NORMAL";
-static const int TODO_TYPE = 2;
+static NSString *APPROVE_PROPERTY_WORKFLOW_ID=@"workflow_id";
+static NSString *APPROVE_PROPERTY_RECORD_ID=@"record_id";
+
+//是否超时
+static NSString *APPROVE_PROPERTY_IS_LATE=@"is_late";
+
+//工作流名称
+static NSString *APPROVE_PROPERTY_WORKFLOW_NAME=@"workflow_name";
+
+//工作流描述
+static NSString *APPROVE_PROPERTY_WORKFLOW_DESC=@"workflow_desc";
+
+//当前节点
+static NSString *APPROVE_PROPERTY_NODE_NAME=@"node_name";
+
+//申请人
+static NSString *APPROVE_PROPERTY_EMPLOYEE_NAME=@"employee_name";
+
+//提交时间
+static NSString *APPROVE_PROPERTY_CREATION_DATE=@"creation_date";
+
+//处理时限
+static NSString *APPROVE_PROPERTY_DATE_LIMIT=@"date_limit";
+
+//本地状态
+static NSString *APPROVE_PROPERTY_LOCAL_STATUS=@"localStatus";
+
+//审批意见
+static NSString *APPROVE_PROPERTY_COMMENT=@"comment";
+
+//审批动作类型
+static NSString *APPROVE_PROPERTY_APPROVE_ACTION_TYPE=@"approveActionType";
 
 @interface Approve : NSObject{
-    NSInteger workflowId;
+    NSUInteger workflowId;
+    NSUInteger recordId;
     NSString *workflowName;//工作流名称
-    NSString *currentStatus;//当前节点
-    NSString *applicant;//申请人
-    NSString *commitDate;//提交时间
-    NSString *deadLine;//处理时限
-    NSString *type;//催办还是代办
+    NSString *workflowDesc;//
+    NSString *nodeName;//当前节点
+    NSString *employeeName;//申请人
+    NSString *creationDate;//提交时间
+    NSString *dateLimit;//处理时限
+    NSString *isLate;//催办还是代办
+    NSString *localStatus;//本地状态，分三类：NORMAL:正常，从服务器取得的未处理数据；WAITING:用户已处理，但未提交到服务器；Done:已提交数据，并且服务器返回ok；ERROR:错误，服务器返回的错误状态
+    NSString *comment;//审批意见
+    NSString *approveActionType ;//审批动作类型
+    
 }
 
-@property(nonatomic) NSInteger workflowId;
+@property(nonatomic) NSUInteger workflowId;
+@property(nonatomic) NSUInteger recordId;
 @property(copy, nonatomic) NSString *workflowName;
-@property(copy, nonatomic) NSString *currentStatus;
-@property(copy, nonatomic) NSString *applicant;
-@property(copy, nonatomic) NSString *commitDate;
-@property(copy, nonatomic) NSString *deadLine;
-@property(copy, nonatomic) NSString *type;
+@property(copy, nonatomic) NSString *workflowDesc;
+@property(copy, nonatomic) NSString *nodeName;
+@property(copy, nonatomic) NSString *employeeName;
+@property(copy, nonatomic) NSString *creationDate;
+@property(copy, nonatomic) NSString *dateLimit;
+@property(copy, nonatomic) NSString *isLate;
+@property(copy, nonatomic) NSString *localStatus;
+@property(copy, nonatomic) NSString *comment;
+@property(copy, nonatomic) NSString *approveActionType;
 
 
 -(Approve *)initWithWorkflowId:(NSInteger)wid workflowName:(NSString *)wName currentStatus:(NSString *)currentStatus applicant:(NSString *)applicant deadLine:(NSString *)deadLine commitDate:(NSString *)commitDate todoType:(NSString *)tType;
+
+-(Approve *)initWithDictionary:(NSMutableDictionary *)dic;
 @end
--- Projects/hrms/hrms/Approve/Approve.m
+++ Projects/hrms/hrms/Approve/Approve.m
@@ -11,20 +11,30 @@
 @implementation Approve
 
 @synthesize workflowId;
+@synthesize recordId;
 @synthesize workflowName;
-@synthesize currentStatus;
-@synthesize applicant;
-@synthesize commitDate;
-@synthesize deadLine;
-@synthesize type;
+@synthesize workflowDesc;
+@synthesize nodeName;
+@synthesize employeeName;
+@synthesize creationDate;
+@synthesize dateLimit;
+@synthesize isLate;
+@synthesize localStatus;
+@synthesize comment;
+@synthesize approveActionType;
 
 -(void)dealloc{
     [workflowName release];
-    [currentStatus release];
-    [applicant release];
-    [deadLine release];
-    [commitDate release];
-    [type release];
+    [workflowDesc release];
+    [nodeName release];
+    [employeeName release];
+    [creationDate release];
+    [dateLimit release];
+    [isLate release];
+    [localStatus release];
+    [comment release];
+    [approveActionType release];
+    
     [super dealloc];
 }
 
@@ -33,11 +43,31 @@
     if (self = [super init]){
         self.workflowId = wid;
         self.workflowName = wName;
-        self.currentStatus = cStatus;
-        self.applicant = a;
-        self.deadLine = dLine;
-        self.commitDate = cDate;
-        self.type = tType;
+        self.nodeName = cStatus;
+        self.employeeName = a;
+        self.dateLimit = dLine;
+        self.creationDate = cDate;
+        self.isLate = tType;
+    }
+    
+    return self;
+}
+
+-(Approve *)initWithDictionary:(NSMutableDictionary *)dic{
+    if (self = [super init]){
+        
+        self.workflowId = [[dic objectForKey:APPROVE_PROPERTY_WORKFLOW_ID] integerValue];
+        self.recordId = [[dic objectForKey:APPROVE_PROPERTY_RECORD_ID] integerValue];
+        self.workflowName = [dic objectForKey:APPROVE_PROPERTY_WORKFLOW_NAME];
+        self.workflowDesc = [dic objectForKey:APPROVE_PROPERTY_WORKFLOW_DESC];
+        self.nodeName = [dic objectForKey:APPROVE_PROPERTY_NODE_NAME];
+        self.employeeName = [dic objectForKey:APPROVE_PROPERTY_EMPLOYEE_NAME];
+        self.creationDate = [dic objectForKey:APPROVE_PROPERTY_CREATION_DATE];
+        self.dateLimit = [dic objectForKey:APPROVE_PROPERTY_DATE_LIMIT];
+        self.isLate = [dic objectForKey:APPROVE_PROPERTY_IS_LATE];
+        self.localStatus = [dic objectForKey:APPROVE_PROPERTY_LOCAL_STATUS];
+        self.comment = [dic objectForKey:APPROVE_PROPERTY_COMMENT];
+        self.approveActionType = [dic objectForKey:APPROVE_PROPERTY_APPROVE_ACTION_TYPE];
     }
     
     return self;
