5cdea6e95a69eeae026c11a113ddf2e6ab800ffd
Yi Zhang <zhemerson@gmail.com>
2012-03-30 16:55:46 +0800

    修改界面

--- Projects/hrms/hrms/Approve/ApproveListCell.h
+++ Projects/hrms/hrms/Approve/ApproveListCell.h
@@ -15,27 +15,24 @@
 
 #define CELL_BOLDFONT(s) [UIFont fontWithName:@"Helvetica-BoldOblique" size:s]
 
-static const int const WORKFLOW_TEXTVIEW_TAG = 1;
-static const int const CURRENTSTATUS_TEXTVIEW_TAG = 2;
-static const int const APPLICANT_TEXTVIEW_TAG = 3;
-static const int const COMMITDATE_TEXTVIEW_TAG = 4;
-static const int const DEADLINE_TEXTVIEW_TAG = 5;
-static const int const TYPEIMG_IMAGEVIEW_TAG = 6;
+static const int const TAG_WORKFLOW_LABEL = 1;
+static const int const TAG_CURRENTSTATUS_LABEL = 2;
+static const int const TAG_TYPEIMGVIEW = 3;
+static const int const TAG_COMMITDATE_LABEL = 4;
+static const int const TAG_DEADLINE_LABEL = 5;
+static const int const TAG_TYPEIMG_IMAGEVIEW = 6;
 
 
 @interface ApproveListCell : UITableViewCell{
-    Approve *approveData;
     
-    UILabel *workflowTextView;
+    UILabel *workflowNameLabel;
+    UIView *alertBackgroundView;
     UILabel *currentStatusTextView;
-    UILabel *applicantTextView;
     UILabel *commitDateTextView;
     UILabel *deadLineTextView;
     UIImageView *typeImg;
     
 }
-
-@property (retain, nonatomic)Approve *approveData;
 
 -(void)setCellData:(Approve *)approveEntity;
 @end
