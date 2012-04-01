aa3d96a29bdb6e4de20a430e7fb9bed8c8eb39f2
XiaoLei Li <707913055@qq.com>
2012-04-01 10:27:20 +0800

    修改登陆模型

--- Projects/hrms/hrms/Login/LoginModel.h
+++ Projects/hrms/hrms/Login/LoginModel.h
@@ -7,15 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "HDLoginDelegate.h"
+#import "HDFormDataRequestDelegate.h"
+
+static NSString * kLoginURLPath =  @"http://172.20.0.20:8080/hr_new/login.svc";
 
 @interface LoginModel : NSObject{
-
+    id <HDLoginDelegate> delegate;
 }
 
+@property (assign) SEL loginSuccessSelector;
+@property (assign) SEL loginFailedSelector;
+
+@property (nonatomic,retain) HDFormDataRequest * loginRequest;
 @property (nonatomic,retain) NSString * username;
 @property (nonatomic,retain) NSString * password;
-@property (nonatomic,retain) NSString * languageDisplay;
-@property (nonatomic,retain) NSString * languageValue;
+@property (nonatomic,assign) id <HDLoginDelegate> delegate;
 
--(id) toDataSet;
+-(void)login;
+
 @end
+
--- Projects/hrms/hrms/Login/LoginModel.m
+++ Projects/hrms/hrms/Login/LoginModel.m
@@ -7,30 +7,91 @@
 //
 
 #import "LoginModel.h"
+#import "HDFunctionUtil.h"
 
 @implementation LoginModel
 
-@synthesize username;
-@synthesize password;
-@synthesize languageDisplay;
-@synthesize languageValue;
+//登陆请求
+@synthesize loginRequest=_loginRequest;
+//登陆post数据
+@synthesize username = _username;
+@synthesize password = _password;
 
--(id) toDataSet{
-//    NSString * s = [[NSUserDefaults standardUserDefaults] stringForKey:@"deviceToken"];
-//    NSLog(@"%@", s);
-    return  [NSDictionary dictionaryWithObjectsAndKeys:
+//成功与失败的回调
+@synthesize loginSuccessSelector;
+@synthesize loginFailedSelector;
+
+//登陆协议 
+@synthesize delegate;
+
+-(void)dealloc
+{
+    [self.loginRequest clearDelegatesAndCancel];
+    TT_RELEASE_SAFELY(_loginRequest);
+    TT_RELEASE_SAFELY(_username);
+    TT_RELEASE_SAFELY(_password);
+    [super dealloc];
+}
+
+-(id) generateLoginData
+{
+    return  [NSMutableDictionary dictionaryWithObjectsAndKeys:
              [[NSUserDefaults standardUserDefaults] stringForKey:@"deviceToken"],@"device_token",
-             @"hand",@"user_name",
-             @"hand",@"user_password",
+             @"admin",@"user_name",
+             @"admin",@"user_password",
              @"简体中文",@"langugae",
              @"ZHS",@"user_language",
              @"N",@"is_ipad", 
+             @"1",@"company_id",
+             @"41",@"role_id",
              nil];
 }
 
--(void)dealloc{
-    TT_RELEASE_SAFELY(username);
-    TT_RELEASE_SAFELY(password);
-    [super dealloc];
+-(void)login
+{
+    self.loginRequest  = [HDFormDataRequest hdRequestWithURL:kLoginURLPath 
+                                                    withData:[self generateLoginData]
+                                                     pattern:HDrequestPatternNormal];
+    //注册回调
+    [_loginRequest setSuccessSelector:@selector(successSelector:dataSet:)];
+    [_loginRequest setServerErrorSelector:@selector(errorSelector:errorMessage:)];
+    [_loginRequest setErrorSelector:@selector(errorSelector:errorMessage:)];
+    [_loginRequest setAsiFaildSelector:@selector(errorSelector:errorMessage:)];
+    
+    [[_loginRequest setDelegate:self]startAsynchronous];
 }
+
+//取消登陆
+-(void)cancelLogin
+{
+    [self.loginRequest cancel];
+}
+
+- (void)successSelector:(ASIFormDataRequest *) request  dataSet:(NSArray *)dataSet
+{
+    SEL function = [HDFunctionUtil matchPerformDelegate:self.delegate 
+                                           forSelectors:loginSuccessSelector,
+                    @selector(loginSuccess:),nil];
+    if (function!=nil) {
+        [delegate performSelector:function
+                       withObject:dataSet];
+    }else {
+        NSLog(@"没有匹配的回调函数");
+    }
+}
+
+- (void)errorSelector:(ASIFormDataRequest *)request errorMessage: (NSString *)errorMessage
+{    
+    SEL function = [HDFunctionUtil matchPerformDelegate:self.delegate 
+                                           forSelectors:loginFailedSelector,
+                    @selector(loginFailed:),nil];
+    
+    if (function!=nil) {
+        [delegate performSelector:function
+                       withObject:errorMessage];
+    }else {
+        NSLog(@"没有匹配的回调函数");
+    }
+}
+
 @end
